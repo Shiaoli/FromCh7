35897ce1a812f909e4eb40805a526812f57d985a
Shiaoli <ntmxl5288@gmial.com>
2018-08-29 10:04:16 -0500

    IEnumerator and IEnumeratable

--- FromCh7/Program.cs
+++ FromCh7/Program.cs
@@ -1,12 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FromCh7
 {
+    public class MusicTitle
+    {
+        string[] names = { "name1", "name2", "name3", "name4" };
+        public IEnumerator<string> GetEnumerator()
+        {
+            for(int i = 0; i < 4; i++)
+            {
+                yield return names[i];
+            }
+        }
+
+        public IEnumerable<string> Resverse()
+        {
+            for(int i = 3; i >= 0; i--)
+            {
+                yield return names[i];
+            }
+        }
+        public IEnumerable<string> Subset(int index, int length)
+        {
+            for(int i=index-1;i<4 && i < (index + length); i++)
+            {
+                yield return names[i];
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program pro = new Program();
+            pro.MusicTitlePractice();
+        }
+        public void MusicTitlePractice()
+        {
+            MusicTitle music = new MusicTitle();
+            foreach(var i in music)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Reserve");
+            foreach(var i in music.Resverse())
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Subset");
+            foreach(var i in music.Subset(2, 4))
+            {
+                Console.WriteLine(i);
+            }
+
+
         }
     }
 }
